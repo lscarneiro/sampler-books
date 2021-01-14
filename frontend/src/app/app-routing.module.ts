2ca0943d934865c0784541d1e8d042a6ccf8aab9
Luiz Eduardo <servergta@gmail.com>
2021-01-13 21:43:42 -0300

    set main routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/core/guards';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => LoginModule,
+  },
+  {
+    path: 'register',
+    loadChildren: () => RegisterModule,
+  },
+  {
+    path: 'home',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    loadChildren: () => HomeModule,
+  },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
