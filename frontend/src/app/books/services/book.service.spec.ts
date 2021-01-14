0274c8f612d4ba5ac6e5d120c8cf83e9edd3ba83
Luiz Eduardo <servergta@gmail.com>
2021-01-14 01:38:40 -0300

    add missing test for book service

--- frontend/src/app/books/services/book.service.spec.ts
+++ frontend/src/app/books/services/book.service.spec.ts
@@ -28,7 +28,7 @@ describe('BookService', () => {
       title: 'Title',
       isbn: '0005534186',
       published_at: '2020-01-01',
-      status: 'AVAILABLE'
+      status: 'AVAILABLE',
     };
 
     service.search({ isbn: 'test' }).subscribe((books) => {
@@ -66,7 +66,26 @@ describe('BookService', () => {
     httpMock
       .expectOne((req) => req.url.endsWith('/books/1/checkout'))
       .flush({
-        "message": "Book checkout successful, enjoy your reading!"
+        message: 'Book checkout successful, enjoy your reading!',
       });
+  });
+
+  it('should list user books', (done) => {
+    const sampleBook = {
+      id: 1,
+      title: 'Title',
+      isbn: '0005534186',
+      published_at: '2020-01-01',
+      status: 'CHECKED_OUT',
+    };
+    service.list().subscribe((books) => {
+      expect(books).not.toBeNull();
+      expect(books.length).toBe(1);
+      expect(books).toContain(sampleBook);
+
+      done();
+    });
+
+    httpMock.expectOne((req) => req.url.endsWith('/books')).flush([sampleBook]);
   });
 });
