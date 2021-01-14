4076d2744a3a31242f87835bb4d6d41f9c0b8d65
Luiz Eduardo <servergta@gmail.com>
2021-01-13 21:36:59 -0300

    add core module

---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
