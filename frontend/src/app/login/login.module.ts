b49e7ec72b8833d6e793a0741d53c1781259bca8
Luiz Eduardo <servergta@gmail.com>
2021-01-15 09:43:37 -0300

    refactor login inputs

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InputModule } from '../shared/components/input/input.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, SharedModule, LoginRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, SharedModule, LoginRoutingModule, ReactiveFormsModule, InputModule],
 })
 export class LoginModule {}
